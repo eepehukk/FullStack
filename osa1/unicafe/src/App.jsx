6efda0c02473c4cbcde8a43817cc0c87f55f1e86
eepehukk <eemil.p01.hukkanen@student.jyu.fi>
2023-09-20 15:33:43 +0300

    Finishing part1

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,34 +1,83 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({handleClick, title}) => {
+  return (
+    <button title={title} onClick={handleClick}>
+      {title}
+    </button>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.all === 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        No feedback given yet
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <StatisticLine text="Good:" value={props.good} />
+          <StatisticLine text="Neutral:" value={props.neutral}/>
+          <StatisticLine text="Bad:" value={props.bad} />
+          <StatisticLine text="All:" value={props.good + props.neutral + props.bad} />
+          <StatisticLine text="Avg:" value={(props.good - props.bad ) / (props.all)} />
+          <StatisticLine text="Positive:" value={props.good / (props.all)* 100 + '%' } />
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>
+        {props.text} 
+      </td>
+      <td>
+        {props.value}
+      </td>
+    </tr>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleGood = () => {
+    console.log('succesfully logged + ', good)
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+  const handleBad = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Give feedback</h1>
+
+      <Button handleClick = {handleGood} title = {'Good'}/>
+      <Button handleClick = {handleNeutral} title = {'Neutral'}/>
+      <Button handleClick = {handleBad} title = {'Bad'}/>      
+
+      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
+    </div>
   )
 }
 
