60de66cda269946cafb0aadd4a9808bc381173b0
Eemil Hukkanen <eemilhukkanen@Mac.lan>
2025-09-10 10:23:00 +0300

    Viimeistellään osaa 1.5

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,9 +1,10 @@
+
 const Header = (props) => {
   console.log(props)
   return (
     <div>
       <h1>
-        {props.course}
+        {props.course.name}
       </h1>
     </div>
   )
@@ -25,7 +26,7 @@ const Content = (props) => {
     <div>
       <Part parts={props.parts[0]}/>
       <Part parts={props.parts[1]}/>
-      <Part parts={props.parts[2]}/>
+      <Part parts={props.parts[2]}/>     
     </div>
   )
 }
@@ -42,17 +43,22 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [{name: 'Fundamentals of React', exercises: 10}, {name: 'Using props to pass data', exercises: 7}, {name: 'State of a component', exercises: 14} ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {name: 'Fundamentals of React', exercises: 10}, 
+      {name: 'Using props to pass data', exercises: 7}, 
+      {name: 'State of a component', exercises: 14} 
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-
-      <Content parts={parts} />
-
-      <Total parts={parts} />
-
+      
+      <Content parts={course.parts} />
+     
+      <Total parts={course.parts} />
     </div>
   )
 }
