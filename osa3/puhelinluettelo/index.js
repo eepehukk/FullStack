2d41a45957cae4536e9a00154dc7a0cc746220fa
Eemil Hukkanen <eemilhukkanen@Eemils-MacBook-Air-4.local>
2025-09-19 11:19:58 +0300

    3.3 tehty

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -47,6 +47,12 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} people</p> <p> ${date} </p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find ( person => person.id === id)
+  response.json(person)
+})
+
 // EI TULE MUUUTTUMAAAN
 const PORT = 3001
 app.listen(PORT, () => {
