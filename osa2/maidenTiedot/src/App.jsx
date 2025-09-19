aafb15243224ac557205a5999427d5ece65550a6
Eemil Hukkanen <eemilhukkanen@Eemils-MacBook-Air-4.local>
2025-09-19 09:18:20 +0300

    Nappi lisätty monen maan esitys kohtaan

--- osa2/maidenTiedot/src/App.jsx
+++ osa2/maidenTiedot/src/App.jsx
@@ -10,7 +10,11 @@ const CountryList = ({ countries }) => {
     return (
       <ul>
         {countries.map(country => (
-          <li key={country.ccn3}>{country.name.common}</li>
+          <li key={country.ccn3}>{country.name.common}
+            <button onClick={() => handleShowCountry(country.name.common)}>
+              show
+            </button>
+          </li>
         ))}
       </ul>
     )
