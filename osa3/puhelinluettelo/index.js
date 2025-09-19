a0a1a63c4c8f115c42685c07bc504b0302c8be15
Eemil Hukkanen <eemilhukkanen@Eemils-MacBook-Air-4.local>
2025-09-19 11:06:00 +0300

    3.2 vaihe alkuvaiheessa

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,1 +1,52 @@
-console.log('hello world')
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+      name: "Arto Hellas",
+      number: "040-123456",
+      id: "1"
+    },
+    {
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+      id: "2"
+    },
+    {
+      name: "Dan Abramov",
+      number: "12-43-234345",
+      id: "3"
+    },
+    {
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+      id: "4"
+    },
+    {
+      id: "004e",
+      name: "Rontti",
+      number: "050-4904094"
+    },
+    {
+      id: "004f",
+      name: "Rontti1000",
+      number: "050-4904094"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p> <p> TESTI </p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
