2a925d6af7b06feffbf55d2d196f64944ebb2694
Eemil Hukkanen <eemilhukkanen@Mac.lan>
2025-09-20 12:03:04 +0300

    3.8 valmis parhaani mukaan

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-
-app.use(express.json())
+const morgan = require('morgan')
 
 let persons = [
     {
@@ -25,6 +24,21 @@ let persons = [
       id: "4"
     }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
+
+morgan.token('body', function (req, res) { return req.method === 'POST' ? JSON.stringify(req.body) : ''})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -93,6 +107,12 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint Rontti lol' })
+}
+
+app.use(unknownEndpoint)
 
 // EI TULE MUUUTTUMAAAN
 const PORT = 3001
