af1c2f44a73b0d99b658cf37a37c1f2eca4e51ad
Eemil Hukkanen <eemilhukkanen@Eemils-MacBook-Air-4.local>
2025-10-07 14:26:43 +0300

    4.8 done, vaikka promiseja ei tullut vastaan vaikka teki materiaalin mukaan

---
+++ osa4/blogilista/tests/blogi_api.test.js
@@ -0,0 +1,51 @@
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const testBlogs = [
+  {
+    "_id": "68e4bdae1cac2b90a74b7977",
+    "title": "Putkivaippaa",
+    "author": "Jänis",
+    "url": "https://example.com/testiblogi",
+    "likes": 25,
+    "__v": 0
+  },
+  {
+    "_id": "68e4c07137565cadd298d308",
+    "title": "Putkivaippaa",
+    "author": "Jänis",
+    "url": "https://example.com/testiblogi",
+    "likes": 25,
+    "__v": 0
+  }
+]
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(testBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(testBlogs[1])
+  await blogObject.save()
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, testBlogs.length)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
