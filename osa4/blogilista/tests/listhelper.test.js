1f16227ee1c94854ad2da75238ec9632914ff7fd
Eemil Hukkanen <eemilhukkanen@Eemils-MacBook-Air-4.local>
2025-10-07 13:20:29 +0300

    4.6 valmis ja 4.7 testicase alustettu

--- osa4/blogilista/tests/listhelper.test.js
+++ osa4/blogilista/tests/listhelper.test.js
@@ -63,3 +63,166 @@ describe('total likes', () => {
 
 /*--------------Favourites----------------------- */
 
+describe('favouriteBlog', () => {
+    const listWithMultipleBlogs = [
+      {
+        _id: '5a422aa71b54a676234d17f9',
+        title: 'Juuso Aaltoon',
+        author: 'Juuso',
+        url: 'http://example.com/a',
+        likes: 100,
+        __v: 0,
+      },
+      {
+        _id: '5a422b3a1b54a676234d17f0',
+        title: 'piip positiiviset puolet',
+        author: 'El Jefe',
+        url: 'http://example.com/b',
+        likes: 7,
+        __v: 0,
+      },
+      {
+        _id: '5a422aa71b54a676234d17f1',
+        title: 'Eemil KANDIDAATIKSI',
+        author: 'Ei kukaan',
+        url: 'http://example.com/L',
+        likes: 100,
+        __v: 0,
+      },
+    ]
+  
+    test('The blog with the most likes', () => {
+      const result = listHelper.favouriteBlog(listWithMultipleBlogs)
+      assert.deepStrictEqual(result, {
+        title: 'Juuso Aaltoon',
+        author: 'Juuso',
+        likes: 100
+      })
+    })
+})
+
+/*--------------mostBlogs----------------------- */
+
+describe('mostBlogs', () => {
+    const listWithMultipleBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+    ]
+  
+    test('The author with most blogs', () => {
+      const result = listHelper.mostBlogs(listWithMultipleBlogs)
+      assert.deepStrictEqual(result, {
+        author: 'Robert C. Martin',
+        blogs: 3
+      })
+    })
+})
+
+/*--------------mostLikes----------------------- */
+
+describe('mostLikes', () => {
+    const listWithMultipleBlogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }  
+    ]
+  
+    test('The author with most likes', () => {
+      const result = listHelper.mostLikes(listWithMultipleBlogs)
+      assert.deepStrictEqual(result, {
+        author: 'Edsger W. Dijkstra',
+        likes: 17
+      })
+    })
+})
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,7 +6,63 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + (blog.likes), 0)
 }
 
+const favouriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    const favourite = blogs.reduce((prev, current) => {
+        if (current.likes > prev.likes) {
+          return current
+        }
+        return prev
+      })
+
+    return {
+        title: favourite.title,
+        author: favourite.author,
+        likes: favourite.likes
+    }
+}
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    // Alustetaan tyhjä muuttuja, jossa sijoitetaan kirjailijan blogimäärä
+    const countByAuthor = {}
+    
+    // Lasketaan kirjailijan blogit
+    blogs.forEach(blog => {
+      if (countByAuthor[blog.author]) {
+        countByAuthor[blog.author] += 1
+      } else {
+        countByAuthor[blog.author] = 1
+      }
+    })
+  
+    // Etsitään kirjoittaja jolla on eniten blogeja
+    let maxBlogs = 0
+    let maxAuthor = ''
+  
+    for (const author in countByAuthor) {
+      if (countByAuthor[author] > maxBlogs) {
+        maxBlogs = countByAuthor[author]
+        maxAuthor = author
+      }
+    }
+  
+    return {
+      author: maxAuthor,
+      blogs: maxBlogs
+    }
+  }
+  
+
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favouriteBlog,
+    mostBlogs
   }
