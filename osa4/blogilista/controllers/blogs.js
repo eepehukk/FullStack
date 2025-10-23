051cc666113bc6038df847943f9cc2d9f4c36ee3
Eemil Hukkanen <eemilhukkanen@Mac.lan>
2025-10-23 15:56:36 +0300

    Vaihe 4.19 ilman 4.16 valmis

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,44 +2,69 @@ const blogsRouter = require('express').Router()
 //const { request, response } = require('../app')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 
-blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
-  response.json(blogs)
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
+    response.json(blogs)
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.post('/', async (request, response) => {
-  const body = request.body
-  const user = await User.findById(body.userId)
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
-  if (!user) {
-    return response.status(400).json({ error: 'userId missing or not valid' })
+    if (!user) {
+      return response.status(400).json({ error: 'userId missing or not valid' })
+    }
+    
+    if (!body.title || !body.url) {
+      return response.status(400).json({ error: 'title or url missing' })
+    }
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes || 0,
+      user: user._id
+    })
+
+    const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    return response.status(201).json(savedBlog.toJSON())
+  } catch (error) {
+    next(error)
   }
-
-  if (!body.title || !body.url) {
-    return response.status(400).json({ error: 'title or url missing' })
-  }
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0,
-    user: user._id
-  })
-
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
-
-  response.status(201).json(savedBlog)
-
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-  response.status(204).end()
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
 // Blogin päivittäminen. tehtävänanto huono, koska ei ota selvää pitääkö vain likejä muokata vai pitääkö muokata kaikkea
